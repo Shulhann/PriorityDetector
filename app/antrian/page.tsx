35b5b121590be49ab0c2101c1e9ce59472afc26d
Shulhan <18221051@std.stei.itb.ac.id>
2024-05-11 15:47:26 +0700

    tambah Header/Footer dan navigasi

--- app/antrian/page.tsx
+++ app/antrian/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="">
-        <h1>halaman registrasi</h1>
+        <h1>halaman antrian</h1>
       </main>
   );
 }
